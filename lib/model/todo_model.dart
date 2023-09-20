afdc606112a84f458e6dfea58f1c7c124e6fa71a
Anand A J <anandjayakumar2001@gmail.com>
2023-09-20 10:09:03 +0530

    intial commit

---
+++ lib/model/todo_model.dart
@@ -0,0 +1,48 @@
+enum Priority { high, medium, low }
+
+class TodoModel {
+  final int id;
+  String task;
+  String? description;
+  DateTime? date;
+  DateTime? time;
+  Priority priority = Priority.low;
+  bool isCompleted = false;
+
+  TodoModel(
+      {required this.id,
+      required this.task,
+      required this.date,
+      required this.time,
+      required this.priority,
+      required this.isCompleted,
+      required this.description}) {
+    description ?? '';
+    print(description);
+    date ?? DateTime.now();
+    time ?? DateTime.now();
+  }
+
+  factory TodoModel.fromMap(Map<String, dynamic> json) {
+    return TodoModel(
+        id: json['id'],
+        task: json['task'],
+        date: json['date'],
+        time: json['time'],
+        priority: json['priority'],
+        isCompleted: json['isCompleted'],
+        description: json['description']);
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'task': task,
+      'date': date,
+      'time': time,
+      'priority': priority,
+      'isCompleted': isCompleted,
+      'description': description
+    };
+  }
+}
